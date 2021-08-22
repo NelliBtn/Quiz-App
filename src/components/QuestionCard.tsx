7392b92f850b5903110b67b85b8610c0a375082f
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-22 15:52:23 -0400

    add type props

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+type Props = {
+  question: string,
+  answer: string[],
+  callback: any,
+  userAnswer: string,
+  questionNumber: number,
+  totalQuestions: number
+}
+
 const QuestionCard = () => (
   <div>Question Card</div>
 );
