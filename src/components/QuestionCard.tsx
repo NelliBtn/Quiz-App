25d048e450f3076f5468985fa9bc058713ac26ab
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-22 16:18:52 -0400

    set up props for question card

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,15 +2,37 @@ import React from 'react';
 
 type Props = {
   question: string,
-  answer: string[],
+  answers: string[],
   callback: any,
-  userAnswer: string,
+  userAnswer: boolean,
   questionNumber: number,
   totalQuestions: number
 }
 
-const QuestionCard = () => (
-  <div>Question Card</div>
+// tell React it is a type of Functional Component, pass props in <>
+const QuestionCard: React.FC<Props> = ({
+  question, 
+  answers, 
+  callback, 
+  userAnswer, 
+  questionNumber, 
+  totalQuestions
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNumber} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{__html: question}} />
+    <div>
+      {answers.map(answer => (
+        <div>
+          <button disabled={userAnswer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{__html: answer}} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
 );
 
 export default QuestionCard;
