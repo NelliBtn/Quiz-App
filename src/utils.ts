e32fbb7ecc5afa80f51aad8644d52d404ea3f111
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-22 17:50:11 -0400

    function to shuffle the array of answers

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,5 @@
-export {}
+
+// shuffle the array of answers so that the correct answer is not always on the same place
+export const shuffleArray = (array: any[]) => {
+  return [...array].sort(() => Math.random() - 0.5);
+}
