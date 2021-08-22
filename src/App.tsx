763a200d5f64d3b5ef1b1feb268fd99b6ce993e4
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-22 18:28:37 -0400

    implement next question function

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,14 @@ const App = () => {
   }
 
   const nextQuestion = () => {
-
+    // move on to the next question if not the
+    const nextQuestion = number + 1;
+    // 
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
   }
 
   return (
