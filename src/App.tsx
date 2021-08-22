9979345bbcbe7879fea9916ef3e8b3c0a7e8808e
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-22 15:47:42 -0400

    component set up and event handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import QuestionCard from './components/QuestionCard';
 
-function App() {
+
+const App = () => {
+
+  const startTrivia = async() => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
-    <div className="App">Quiz App</div>
+    <div className="App">
+      <h1>Quiz</h1>
+      <button className="start" onClick={startTrivia}>Start game</button>
+      <p className="score">Score: </p>
+      <p className="loading">Loading questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>Next question</button>
+    </div>
   );
 }
 
